3edc226786cf8f473822fc837301fd9c70d6083e
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-02 20:11:40 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,8 @@
-def count_elements(array)
+require 'pry'
+
+def count_elements(arr)
   # code goes here
+  new_hash = {}
+  arr.each {|pet| new_hash[pet] ? new_hash[pet] +=1 : new_hash[pet] = 1}
+  new_hash
 end
- 
